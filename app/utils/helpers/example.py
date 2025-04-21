8a40972369191b0ed2dc1494b3b02639a76a7aff
Felix Evers <git@felixevers.de>
2025-04-22 00:23:42 +0200

    chore: add more products to example data

--- app/utils/helpers/example.py
+++ app/utils/helpers/example.py
@@ -16,42 +16,87 @@ def create_example_data():
     if not session.query(Product).first():
         logging.info("Initializing app with example data set")
 
-        product = Product(
+        product_azd = Product(
             name="App Zum Doc",
             description="Kommunizieren Sie mit Ihren Patienten!",
             image_url="https://app-zum-doc.de/images/appzumdoc_20222.png",
         )
-        session.add(product)
-        session.commit()
-        session.refresh(product)
 
-        plan1 = ProductPlan(
-            product_uuid=product.uuid,
+        product_netzmanager = Product(
+            name="mediQuu Netzmanager",
+            description="Verwaltungstool für Arztnetze und Praxisverbunde.",
+            image_url="https://login.mediquu.de/assets/images/mediquu_logo_2022.png",
+        )
+
+        product_viva = Product(
+            name="mediQuu viva",
+            description="Die Patientenakte für Zusatzleistungen (iSd. SGB V §140a) synchronisiert mit allen teilnehmenden Praxen.",
+        )
+
+        session.add(product_azd)
+        session.add(product_netzmanager)
+        session.add(product_viva)
+        session.commit()
+        session.refresh(product_azd)
+        session.refresh(product_netzmanager)
+        session.refresh(product_viva)
+
+        azd_plan1 = ProductPlan(
+            product_uuid=product_azd.uuid,
             name="monatlich",
             type=PlanTypeEnum.RECURRING,
             recurring_month=1,
             cost_euro=199.99,
         )
-        plan2 = ProductPlan(
-            product_uuid=product.uuid,
+        azd_plan2 = ProductPlan(
+            product_uuid=product_azd.uuid,
             name="jährlich",
             type=PlanTypeEnum.RECURRING,
             recurring_month=12,
             cost_euro=1999.99,
         )
 
-        session.add(plan1)
-        session.add(plan2)
+        netzmanager_plan1 = ProductPlan(
+            product_uuid=product_netzmanager.uuid,
+            name="jährlich",
+            type=PlanTypeEnum.RECURRING,
+            recurring_month=12,
+            cost_euro=17040.00,
+        )
+
+        viva_plan1 = ProductPlan(
+            product_uuid=product_viva.uuid,
+            name="monatlich",
+            type=PlanTypeEnum.RECURRING,
+            recurring_month=1,
+            cost_euro=800.00,
+        )
+        viva_plan2 = ProductPlan(
+            product_uuid=product_viva.uuid,
+            name="Analytics monatlich",
+            type=PlanTypeEnum.RECURRING,
+            recurring_month=1,
+            cost_euro=1200.00,
+        )
+
+        session.add(azd_plan1)
+        session.add(azd_plan2)
+        session.add(netzmanager_plan1)
+        session.add(viva_plan1)
+        session.add(viva_plan2)
         session.commit()
-        session.refresh(plan1)
-        session.refresh(plan2)
+        session.refresh(azd_plan1)
+        session.refresh(azd_plan2)
+        session.refresh(netzmanager_plan1)
+        session.refresh(viva_plan1)
+        session.refresh(viva_plan2)
 
         now = datetime.now()
 
         voucher = Voucher(
             code="TEST10",
             description="Free for you! <3",
-            product_plan_uuid=plan1.uuid,
+            product_plan_uuid=azd_plan1.uuid,
             discount_percentage=100,
             valid_from=now,
             valid_until=now + timedelta(days=7),
@@ -60,19 +105,60 @@ def create_example_data():
         session.add(voucher)
         session.commit()
 
-        contract = Contract(
+        version = "0460675bb237984e211344881df9f611cde2ff74"
+
+        contract_agb_azd = Contract(
             key=ContractKeyEnum.AGB_APP_ZUM_DOC,
-            version="b60cf20350af5d0f8c037aac3267c9a2593f99cd",
-            url="https://cdn.legal.helpwave.de/main/agb_app_zum_doc.pdf",
+            version=version,
+            url=f"https://cdn.legal.helpwave.de/{version}/agb_app_zum_doc.pdf",
         )
 
-        session.add(contract)
+        contract_avv_global = Contract(
+            key=ContractKeyEnum.AVV,
+            version=version,
+            url=f"https://cdn.legal.helpwave.de/{version}/avv.pdf",
+        )
+
+        contract_sub_global = Contract(
+            key=ContractKeyEnum.SUB,
+            version=version,
+            url=f"https://cdn.legal.helpwave.de/{version}/sub.pdf",
+        )
+
+        contract_agb_netzmanager = Contract(
+            key=ContractKeyEnum.AGB_MEDIQUU_NETZMANAGER,
+            version=version,
+            url=f"https://cdn.legal.helpwave.de/{version}/agb_netzmanager.pdf",
+        )
+
+        session.add(contract_agb_azd)
+        session.add(contract_avv_global)
+        session.add(contract_sub_global)
+        session.add(contract_agb_netzmanager)
         session.commit()
-        session.refresh(contract)
+        session.refresh(contract_agb_azd)
+        session.refresh(contract_avv_global)
+        session.refresh(contract_sub_global)
+        session.refresh(contract_agb_netzmanager)
 
-        product_contract = ProductContract(
-            product_uuid=product.uuid, contract_uuid=contract.uuid
+        product_contract_agb_azd = ProductContract(
+            product_uuid=product_azd.uuid, contract_uuid=contract_agb_azd.uuid
         )
 
-        session.add(product_contract)
+        product_contract_avv_global = ProductContract(
+            product_uuid=product_azd.uuid, contract_uuid=contract_avv_global.uuid
+        )
+
+        product_contract_sub_gloabl = ProductContract(
+            product_uuid=product_azd.uuid, contract_uuid=contract_sub_global.uuid
+        )
+
+        product_contract_agb_netzmanager = ProductContract(
+            product_uuid=product_azd.uuid, contract_uuid=contract_agb_netzmanager.uuid
+        )
+
+        session.add(product_contract_agb_azd)
+        session.add(product_contract_avv_global)
+        session.add(product_contract_sub_gloabl)
+        session.add(product_contract_agb_netzmanager)
         session.commit()
